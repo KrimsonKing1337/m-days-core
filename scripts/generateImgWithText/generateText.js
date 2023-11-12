e4aa387f68dfa9d54fdaa3801906f4569f489753
Oleg Shilov <shilov-1@yandex.ru>
2023-11-12 19:10:04 +0300

    wip: generate image with text due sharp.js;

--- scripts/generateImgWithText/generateText.js
+++ scripts/generateImgWithText/generateText.js
@@ -5,7 +5,7 @@ async function main() {
 
   // current date
   // adjust 0 before single digit date
-  let date = ('0' + date_ob.getDate()).slice(-2);
+  let day = ('0' + date_ob.getDate()).slice(-2);
 
   // current month
   let month = ('0' + (date_ob.getMonth() + 1)).slice(-2);
@@ -22,35 +22,55 @@ async function main() {
   // current seconds
   let seconds = date_ob.getSeconds();
 
-  let width = 200;
-  let height = 100;
-  let label = `${hours}:${minutes}`;
+  const width = 200;
+  const height = 100;
+
+  const timeLabel = `${hours}:${minutes}`;
+  const dateLabel = `${day}.${month}.${year}`;
+
+  console.log('___', dateLabel);
 
   const image = await sharp('./1.jpg');
   const metadata = await image.metadata()
 
-  const bg = Buffer.from(`
+  const shadow = Buffer.from(`
     <svg height="${metadata.height}" width="${metadata.width}">
       <rect x="0" y="0" width="100%" height="100%" fill="#000" fill-opacity="0.3" />
     </svg>
   `);
 
-  const svg = Buffer.from(`
-    <svg height="${height}" width="${width}">
-      <text x="50%" y="50%" text-anchor="middle" dy="0.4em" font-size="${width / 4}" fill="#fff" font-family="sans">
-        ${label}
+  const timeHeight = metadata.height / 2;
+
+  const time = Buffer.from(`
+    <svg height="${timeHeight}" width="${metadata.width}">
+      <text x="50%" y="50%" text-anchor="middle" dy="0.4em" font-size="${timeHeight / 2.5}" fill="#fff" font-family="sans">
+        ${timeLabel}
       </text> 
     </svg>
-`);
+  `);
+
+  const dateHeight = metadata.height / 1.6;
+
+  const date = Buffer.from(`
+    <svg height="${dateHeight}" width="${metadata.width}">
+      <text x="50%" y="50%" text-anchor="middle" dy="0.4em" font-size="${dateHeight / 6}" font-stretch="ultra-condensed" fill="#fff" font-family="sans">
+        ${dateLabel}
+      </text> 
+    </svg>
+  `);
 
   const compositeImages = [
     {
-      input: bg,
-      gravity: 'centre'
+      input: shadow,
+      gravity: 'center'
     },
     {
-      input: svg,
-      gravity: 'center',
+      input: time,
+      gravity: 'center'
+    },
+    {
+      input: date,
+      gravity: 'south'
     }
   ];
 
