4dd0e0f4abcfa5397274b007f5916205a54b0999
Oleg Shilov <shilov-1@yandex.ru>
2023-11-28 15:26:44 +0300

    added new font; updated build;

--- lib/src/api.d.ts
+++ lib/src/api.d.ts
@@ -2,6 +2,7 @@ import { WeatherResp } from './@types';
 export type getCurrentWeatherParams = {
     latitude: number;
     longitude: number;
+    protocol?: string;
 };
 export declare function getCurrentWeather({ latitude, longitude }: getCurrentWeatherParams): Promise<WeatherResp>;
 export declare function getHourlyWeather({ latitude, longitude }: getCurrentWeatherParams): Promise<WeatherResp>;
