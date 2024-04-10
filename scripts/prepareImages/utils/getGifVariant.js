f399e418d1b6b773b9550bfdeb110ee6da381b19
Oleg Shilov <shilov-1@yandex.ru>
2024-04-10 19:36:20 +0300

    added formula for calc variants of gifs;

--- scripts/prepareImages/utils/getGifVariant.js
+++ scripts/prepareImages/utils/getGifVariant.js
@@ -5,10 +5,18 @@
  */
 function getGifVariant({ width, height } = {}) {
   if (width > height) {
+    if (width / height > 1 && width / height < 1.3333) {
+      return 'sq';
+    }
+
     return 'h';
   }
 
   if (width < height) {
+    if (width / height > 0.7 && width / height < 1) {
+      return 'sq';
+    }
+
     return 'v';
   }
 
