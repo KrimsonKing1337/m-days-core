dfa07bdd97217249f092021da28eed2419117fb8
Oleg Shilov <shilov-1@yandex.ru>
2024-04-14 09:13:14 +0300

    cutImages;

---
+++ scripts/prepareImages/cutImages.js
@@ -0,0 +1,48 @@
+const fs = require('fs').promises;
+const path = require('path');
+const sharp = require('sharp');
+
+const { readDirR, removeDir, makeDir } = require('./utils');
+
+const imagesSourcesPath = 'C:\\Users\\K\\Downloads\\pics-test\\static';
+const imagesTargetPath = path.join(imagesSourcesPath, './_ready');
+
+const requiredWidth = 480;
+const requiredHeight = 272;
+
+async function cutImages() {
+  removeDir(imagesTargetPath);
+  makeDir(imagesTargetPath);
+
+  const images = readDirR({
+    path: imagesSourcesPath,
+    formats: ['jpg', 'gif'],
+  });
+
+  for (const imageCur of images) {
+    const meta = await sharp(imageCur.fullPath).metadata();
+
+    const indexStart = imagesSourcesPath.length;
+    const newSubFolder = imageCur.fullPathWithoutName.substring(indexStart);
+
+    const imgCurTargetDir = `${imagesTargetPath}/${newSubFolder}`;
+
+    makeDir(imgCurTargetDir);
+
+    const newFullName = `${imgCurTargetDir}/${imageCur.name}`;
+
+    const { width, height } = meta;
+
+    if (width <= requiredWidth || height <= requiredHeight) {
+      continue;
+    }
+
+    await sharp(imageCur.fullPath)
+      .resize({ width: requiredWidth, height: requiredHeight, fit: 'cover' })
+      .toFile(newFullName);
+  }
+}
+
+cutImages();
+
+module.exports = { cutImages };
