efea5d260408da8cacdd8d67073d3e216582ba3f
Oleg Shilov <shilov-1@yandex.ru>
2024-04-16 11:29:49 +0300

    changed pathes;

--- scripts/prepareImages/generateJsonForImages.js
+++ scripts/prepareImages/generateJsonForImages.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const dir = 'C:\\Users\\K\\Downloads\\pics\\static\\_ready';
+const dir = 'C:\\Users\\K\\Downloads\\pics\\_ready';
 
 function readDirectory(dir) {
   const result = {};
--- scripts/prepareImages/utils/getPaths.js
+++ scripts/prepareImages/utils/getPaths.js
@@ -7,11 +7,13 @@ const photosTargetPath = path.join(photosSourcesPath, './_ready');
 const photosTempPath = path.join(photosSourcesPath, './_temp');
 
 const imagesSourcesPath = 'C:\\Users\\K\\Downloads\\pics\\_ready_random\\static';
-const imagesTargetPath = 'C:\\Users\\K\\Downloads\\pics\\static\\_ready';
+// const imagesTargetPath = 'C:\\Users\\K\\Downloads\\pics\\static\\_ready';
+const imagesTargetPath = 'C:\\Users\\K\\Downloads\\pics\\_ready\\static';
 const imagesTempPath = 'C:\\Users\\K\\Downloads\\pics\\static\\_temp';
 
 const gifsSourcesPath = 'C:\\Users\\K\\Downloads\\pics\\_ready_random\\dynamic';
-const gifsTargetPath = 'C:\\Users\\K\\Downloads\\pics\\dynamic\\_ready';
+// const gifsTargetPath = 'C:\\Users\\K\\Downloads\\pics\\dynamic\\_ready';
+const gifsTargetPath = 'C:\\Users\\K\\Downloads\\pics\\_ready\\dynamic';
 const gifsTempPath = 'C:\\Users\\K\\Downloads\\pics\\dynamic\\_temp';
 
 const randomImagesSourcesPath = 'C:\\Users\\K\\Downloads\\pics';
