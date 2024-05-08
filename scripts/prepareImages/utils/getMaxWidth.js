43f4b58fafead44621439fd6286f335c478e3a9b
Oleg Shilov <shilov-1@yandex.ru>
2024-05-08 10:52:33 +0300

    added extra widths for getMaxWidth function;

--- scripts/prepareImages/utils/getMaxWidth.js
+++ scripts/prepareImages/utils/getMaxWidth.js
@@ -4,14 +4,18 @@
 function getMaxWidth(width) {
     if (typeof width === 'string') width = parseInt(width);
 
+    if (width >= 128 && width < 240) return 128;
+    if (width >= 240 && width < 360) return 240;
+    if (width >= 360 && width < 480) return 360;
+    if (width >= 480 && width < 640) return 480;
     if (width >= 640 && width < 1280) return 640;
-    else if (width >= 1280 && width < 1600) return 1280;
-    else if (width >= 1600 && width < 1920) return 1600;
-    else if (width >= 1920 && width < 2560) return 1920;
-    else if (width >= 2560 && width < 3840) return 2560;
-    else if (width >= 3840 && width < 5210) return 3840;
-    else if (width >= 5210 && width < 7680) return 5210;
-    else if (width >= 7680) return 7680;
+    if (width >= 1280 && width < 1600) return 1280;
+    if (width >= 1600 && width < 1920) return 1600;
+    if (width >= 1920 && width < 2560) return 1920;
+    if (width >= 2560 && width < 3840) return 2560;
+    if (width >= 3840 && width < 5210) return 3840;
+    if (width >= 5210 && width < 7680) return 5210;
+    if (width >= 7680) return 7680;
 }
 
 module.exports = { getMaxWidth };
